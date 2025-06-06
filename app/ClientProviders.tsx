73b5d55898638765c9701869c5b7b25898ff4c38
Katherine Zorina <ekaterina.zorina.eneca@gmail.com>
2025-06-06 12:21:25 +0300

    тостер

--- app/ClientProviders.tsx
+++ app/ClientProviders.tsx
@@ -1,18 +1,14 @@
 "use client";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ThemeSync } from "@/components/theme-sync";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <ThemeSync />
       {children}
-      <Toaster 
-        richColors 
-        position="top-right" 
-        className="toaster-with-shadow"
-      />
+      <Toaster />
     </ThemeProvider>
   );
 } 
