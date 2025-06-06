e52007cfce516230b58274efe644c9777b2901d6
Katherine Zorina <ekaterina.zorina.eneca@gmail.com>
2025-06-06 11:21:41 +0300

    Оптимизирована проверка разрешений в компоненте AnnouncementsWidget: удалены лишние зависимости из useMemo для повышения производительности.

--- modules/announcements/components/AnnouncementsWidget.tsx
+++ modules/announcements/components/AnnouncementsWidget.tsx
@@ -32,7 +32,7 @@ export function AnnouncementsWidget() {
   // Проверяем разрешение на создание и редактирование объявлений
   const canCreateAndEdit = useMemo(() => {
     return userStore.hasPermission("announcements_can_create_and_edit")
-  }, [userStore.id, userStore.role, userStore.permissions])
+  }, [userStore.id, userStore.permissions])
 
   // Ждем загрузки разрешений
   useEffect(() => {
